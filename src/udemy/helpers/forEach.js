433fde59558796819e95dc066f7ba1fbfd3dbef9
Sakthivel B <ideamatex@gmail.com>
2021-12-11 01:36:18 +0530

    add eslint

--- src/udemy/helpers/forEach.js
+++ src/udemy/helpers/forEach.js
@@ -1,19 +1,19 @@
 const init = () => {
-    console.log("---for each helper init---");
+    console.log('---for each helper init---')
 
-    const colors = ["red", "green", "blue"];
-    colors.forEach(color => console.log(color));
+    const colors = ['red', 'green', 'blue']
+    colors.forEach(color => console.log(color))
 
-    const numbers = [1, 2, 3, 4, 5, 6];
-    let sum = 0;
-    numbers.forEach(n => sum += n);
-    console.log("sum=", sum);
+    const numbers = [1, 2, 3, 4, 5, 6]
+    let sum = 0
+    numbers.forEach(n => sum += n)
+    console.log('sum=', sum)
 
-    const doubleNumbers = numbers.map(n => n * 2);
-    console.log(">>>doubleNumbers=", doubleNumbers);
+    const doubleNumbers = numbers.map(n => n * 2)
+    console.log('>>>doubleNumbers=', doubleNumbers)
 
-    const evenNumbers = numbers.filter(n => n % 2 === 0);
-    console.log(">>>evenNumbers=", evenNumbers);
-};
+    const evenNumbers = numbers.filter(n => n % 2 === 0)
+    console.log('>>>evenNumbers=', evenNumbers)
+}
 
-export default init;
+export default init
